de453407d9d67bd5c5c42ff39a4081b6efe9b048
Kevin Beltráń <cifuentes.kevin@correounivalle.edu.co>
2021-02-19 21:53:46 -0500

    Versión final cap 2

---
+++ Cap 2/2.6/ParameterizedFragmentShader/src/MyGame/MyGame.js
@@ -0,0 +1,18 @@
+function MyGame(htmlCanvasID) {
+  // The shader for drawing
+  this.mShader = null;
+  gEngine.Core.initializeWebGL(htmlCanvasID);
+  this.mShader = new SimpleShader(
+    "src/GLSLShaders/SimpleVS.glsl",
+    "src/GLSLShaders/SimpleFS.glsl"
+  );
+
+  // Step C: Draw!
+  // Step C1: Clear the canvas
+  gEngine.Core.clearCanvas([0.8, 0, 0, 1]);
+  // Step C2: Activate the proper shader
+  this.mShader.activateShader([255,235, 0, 1]);
+  // Step C3: Draw with the currently activated geometry and the activated shader
+  var gl = gEngine.Core.getGL();
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
